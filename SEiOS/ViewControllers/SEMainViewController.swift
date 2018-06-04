792d58656feee38182c1a27a6191db6400fc6691
ramaraju_gunturu <ramaraju.gunturu@infovisionlabs.com>
2018-06-04 10:54:26 +0530

    File structure has been changed.

---
+++ SEiOS/ViewControllers/SEMainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SEMainViewController.swift
+//  SEiOS
+//
+//  Created by Rama Raju Gunturu on 04/06/18.
+//  Copyright © 2018 InfoVision Labs India Pvt. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class SEMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
