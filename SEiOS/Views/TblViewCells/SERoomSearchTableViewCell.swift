c500c93851986f8ee23dadcb2453e5a14794c218
ramaraju_gunturu <ramaraju.gunturu@infovisionlabs.com>
2018-07-27 16:35:24 +0530

    Here I updated the dummy image for coffee. room.

--- SEiOS/Views/TblViewCells/SERoomSearchTableViewCell.swift
+++ SEiOS/Views/TblViewCells/SERoomSearchTableViewCell.swift
@@ -28,3 +28,35 @@ class SERoomSearchTableViewCell: UITableViewCell {
     
 }
 
+extension UIImageView {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        
+        get{
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+}
+
